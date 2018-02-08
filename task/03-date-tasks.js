d11d95602b06f689264fb0d31cac7520ae2cc2b9
EugeneIvankin <evgeniiivankin@mail.ru>
2018-02-08 21:11:45 +0300

    date-tasks

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,6 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
+   return new Date(value); 
    throw new Error('Not implemented');
 }
 
@@ -37,6 +38,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
+   return new Date(value); 
    throw new Error('Not implemented');
 }
 
@@ -56,6 +58,11 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
+   let year = date.getFullYear();
+   if (year % 4 !== 0) return false;
+   else if (year % 100 !== 0) return true;
+   else if (year % 400 !== 0) return false
+   else return true; 
    throw new Error('Not implemented');
 }
 
@@ -76,6 +83,8 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
+   let miliseconds = (endDate-startDate);
+   //return miliseconds;
    throw new Error('Not implemented');
 }
 
@@ -94,6 +103,17 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
+    let min = date.getMinutes();
+    let hours = date.getHours();
+    let radiansMin = 6*min;
+    let radiansHours = 0.5*(60*hours+min)%360;
+    // if (radiansHours > 360) {
+    //     radiansHours = Math.floor(radiansHours/360);
+    // }
+    if ((Math.abs(radiansHours-radiansMin)*Math.PI/180) > 180){
+        return (Math.abs(radiansHours-radiansMin)*Math.PI/180) -180;
+    }
+    return Math.abs(radiansHours-radiansMin)*Math.PI/180;
     throw new Error('Not implemented');
 }
 
